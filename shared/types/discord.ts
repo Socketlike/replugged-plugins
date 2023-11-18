af26f51abc9a8e0b97c08c624c93a45e300efaf7
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-11-18 18:47:27 +0700

    chore(shared/types/discord): add more types

--- shared/types/discord.ts
+++ shared/types/discord.ts
@@ -21,3 +21,34 @@ export type MenuSliderControl = React.ForwardRefRenderFunction<
   }>,
   MenuSliderControlProps
 >;
+
+export interface TabBarProps<T extends string = string> {
+  'aria-label'?: string;
+  children?: React.ReactNode;
+  className?: string;
+  look?: string;
+  onItemSelect: (newItem: T) => void;
+  orientation?: string;
+  selectedItem: T;
+  style?: React.CSSProperties;
+  type?: string;
+}
+
+export interface TabBarItemProps<T extends string = string> {
+  children?: React.ReactNode;
+  className?: string;
+  clickableRef?: React.MutableRefObject<HTMLElement>;
+  color?: string;
+  disabled?: boolean;
+  disableItemStyles?: boolean;
+  id: T;
+  itemType?: string;
+  look?: string;
+  onContextMenu?: (event: React.MouseEvent) => void;
+  selectedItem?: string;
+}
+
+export interface TabBar {
+  (props: TabBarProps): React.ReactElement;
+  Item: <T extends string = string>(props: TabBarItemProps<T>) => React.ReactElement;
+}
